b82a30f5c450ab6b4eda583c2e9a28e4f28cedd5
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-12-12 13:49:47 +0530

    Add files via upload

---
+++ removefrombottomusingrecursion.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void f(stack<int> &st){
+    if(st.size()==1){
+        st.pop();
+        return;;
+    }
+    int curr = st.top();
+    st.pop();
+    f(st);
+    st.push(curr);
+
+}
+
+int main(){
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    f(st);
+    while(not st.empty()){
+        int curr = st.top();
+        st.pop();
+        cout<<curr<<endl;
+    }
+    
+}
