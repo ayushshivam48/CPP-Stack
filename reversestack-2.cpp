7b6d7e433cc53ac47e2f72fe9f957da0300571d0
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-12-15 10:42:24 +0530

    Add files via upload

---
+++ reversestack-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void reverseStack(stack<int> &st) {
+    stack<int> t1,t2;
+    while(! st.empty()){
+        int curr = st.top();
+        st.pop();
+        t1.push(curr);
+    }
+    while(! t1.empty()){
+        int curr = t1.top();
+        t1.pop();
+        t2.push(curr);
+    }
+    while(! t2.empty()){
+        int curr = t2.top();
+        t2.pop();
+        st.push(curr);
+    }
+}
+int main() {
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    reverseStack(st);
+    while(not st.empty()){
+        int curr = st.top();
+        st.pop();
+        cout<<curr<<endl;
+    
+    }
+    }
