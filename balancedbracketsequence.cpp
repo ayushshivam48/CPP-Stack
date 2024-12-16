02795594efe9cefaf11f7108084502ec0f3e4641
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-12-16 09:36:19 +0530

    Add files via upload

---
+++ balancedbracketsequence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string str){
+    stack<int> st;
+    for(int i=0;i<str.size();i++){
+        char ch = str[i];
+        if(ch=='[' or ch=='{' or ch=='('){
+            st.push(ch);
+        }else{
+            if(ch==')' and !st.empty() and st.top()=='('){
+                st.pop();
+            }else if(ch=='}' and !st.empty() and st.top()=='{'){
+                st.pop();
+            }else if(ch==']' and !st.empty() and st.top()=='['){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+        
+    }return st.empty();
+}
+
+int main() {
+    string str = "()()(())";
+    cout<<isValid(str)<<endl;
+}
